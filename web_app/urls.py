01f7ce2f8711c7fd03978f4816d9a7603a4934be
kinqston34 <aa37741867@gmail.com>
2024-01-22 13:57:15 +0800

    註解 urls

--- web_app/urls.py
+++ web_app/urls.py
@@ -7,35 +7,33 @@ from web_app.views import IndexCardView
 urlpatterns = [
     # TemplateView.as_view (extra_context={'login': "illegal"})
     # url(r"^index/",views.index,name="index"),
-    #======測試======#
+    #===== 測試 =====#
     url(r"^test",views.test,name="test"),
-    
-    #======index======#
+
+    #===== index =====#
     url(r"^index/$",TemplateView.as_view(template_name="index.html"),name="index"),
     url(r"^index/IThome/(?P<type>\w+)?",IndexCardView.as_view(),name= "IThome"),
     url(r"^index/TechNews/(?P<type>\w+)?",IndexCardView.as_view(),name= "TechNews"),
-    
-    #======login======#
+
+    # ==== 登入登出 =====#
     url(r"^login/$",views.login,name="login"),
+    url(r"^login/forget_password/",views.forget_password,name="forget_password"),
+    #url(r"^login/forget_mail_vertified/(\d+)/",views.forget_email_vertified,name="forget_email_vertified"),
+    url(r"^login/forget_email/*",views.send_forget_email,name="forget_email"),
     url(r"^logout/",views.logout,name="logout"),
     
-    #======register======#
-    url(r"^login/?$",views.register_success,name="register_success"),
-    # url(r"^register/(?P<register>)*",views.register,name="register"),
+    # ==== 註冊 =====#
     url(r"^register/",views.register,name="register"),
+    url(r"^login/?$",views.register_success,name="register_success"),
 
-    #======login/forget_password======#
-    url(r"^login/forget_password/(?P<rand>\w+)?",views.forget_password,name="forget_password"),
-    # url(r"^login/forget_mail_vertified/(?P<rand>\w+)/",views.forget_email_vertified,name="forget_email_vertified"),
-    url(r"^login/forget_email/*",views.send_forget_email,name="forget_email"),
-    url(r"^login/reset_password/",views.reset_password,name="reset_password"),
-    
-    #======home/後台 ======#
+    #===== 後台 =====#
     url(r"^home/",views.home,name="home"),
     url(r"^home_list/",views.Homelist.as_view(),name="home_list"),
-   
-    #====== 照片上傳 ======#
+    # url(r"^register/(?P<register>)*",views.register,name="register"),
+    
+    #===== 上傳 =====#
     url(r"^picture/",views.pictureupdate,name="pictureupdate"),
+
 
 ]
 
